f15a563537824d4ff8db44c30d71eedf4792378c
Josh Zylstra <zylstrajosh@gmail.com>
2017-04-08 08:58:18 -0700

    Setting up a model.

---
+++ models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: { type: String, unique: true, lowercase: true },
+  password: String
+})
+const ModelClass = mongoose.model('user', userSchema);
+
+module.exports = ModelClass;
