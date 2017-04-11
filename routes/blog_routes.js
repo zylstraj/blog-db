81da8193126a40032210da1ac8d89a81ee7588f8
Josh Zylstra <zylstrajosh@gmail.com>
2017-04-11 08:10:58 -0700

    GET/POST Working
    GET all and Get a single blog are working, as well as post routes.

--- routes/blog_routes.js
+++ routes/blog_routes.js
@@ -1,13 +1,12 @@
 const Blogs = require('../models/blogs');
 
 exports.getBlogs = function(req, res, next) {
-  console.log(req);
   const title = req.body.title;
   const tagline = req.body.tagline;
   const content = req.body.content;
 
-  Blogs.find({}, (err, req) => {
-    res.json(req.body)
+  Blogs.find({}, (err, blog) => {
+    res.json(blog)
   })
 }
 exports.newBlog = function(req, res, next) {
@@ -20,4 +19,10 @@ exports.newBlog = function(req, res, next) {
   newBlog.save((err, req) => {
     res.json(req.body)
   })
+}
+exports.getOneBlog = function(req, res, next) {
+    Blogs.findById(req.params.id, (err, blog) => {
+      console.log(blog)
+      res.json(blog)
+    })
 }
