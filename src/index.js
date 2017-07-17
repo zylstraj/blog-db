aad81b9fd8630d02114553b63fc630093c459e4b
Josh Zylstra <zylstrajosh@gmail.com>
2017-07-16 21:21:09 -0700

    Working again
    Trying to connect React Router and also - - figure out client app

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactRouter from 'react-router-dom';
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
 // var Router = ReactRouter.BrowserRouter;
 // var Route = ReactRouter.Route;
 
 import App from './components/app';
+
 class SetUp extends React.Component {
   render() {
-    // <Router>
     <div>
-      // <Route path="/blogs" component={App} />
-      <App />
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
     </div>
-    // </Router>
   }
 }
 
 ReactDOM.render(
-  <SetUp />, document.getElementById('container'));
+  <BrowserRouter>
+    <SetUp />
+  </BrowserRouter>
+  , document.getElementById('container'));
